e317029437860cb9f267ffdbc33b65bfa98f7102
Ana Cristina Mendez Velasco <23161030@itoaxaca.edu.mx>
2025-09-10 15:41:48 -0600

    Add files via upload

---
+++ T1.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class T1 {
+    private JPanel Panelprincipal;
+    private JTextField emailTextField;
+    private JTextField contraseñaTextField;
+    private JCheckBox remembermeCheckBox;
+    private JButton iniciarSesiónButton;
+}
